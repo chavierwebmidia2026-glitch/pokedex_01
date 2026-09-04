da26ff1fc5d0eabcf6db449e0f18c48f1cbfac6f
chavierwebmidia2026-glitch <chavierwebmidia2026@gmail.com>
2026-09-03 23:15:30 -0300

    refactor: atualizar view e cards da Pokedex
    Atualização da view responsável pelo consumo e tratamento dos dados da PokéAPI.
    
    Ajustes no componente de cards utilizado na página inicial para melhorar a exibição dos Pokémon recebidos pela API.
    
    Integração e validação das alterações no navegador, mantendo o carregamento dos Pokémon e a apresentação dinâmica dos cards funcionando corretamente.

--- pokemon/views.py
+++ pokemon/views.py
@@ -4,7 +4,29 @@
      < VIEWS := PÁGINA VIEWS.PY >
      ============================ '''
 
+import requests
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'index.html')
+    url = 'https://pokeapi.co/api/v2/pokemon?limit=20&offset=0'
+    response = requests.get(url)
+    data = response.json()
+
+    pokemons = []
+
+    for item in data['results']:
+        detalhes = requests.get(item['url']).json()
+
+        pokemons.append({
+            'id': detalhes['id'],
+            'nome': detalhes['name'],
+            'imagem': detalhes['sprites']['other']['official-artwork']['front_default'],
+            'tipos': [t['type']['name'] for t in detalhes['types']],
+        })
+
+    context = {
+        'pokemons': pokemons,
+    }
+
+    return render(request, 'pokemon/index.html', context)
